59cd582a83e06cc784ee2289fb749b8e1992c18c
Stephan Klinger <staeff@arcor.de>
2015-02-04 21:27:19 +0000

    snapshot part 2

--- app/views.py
+++ app/views.py
@@ -1,7 +1,44 @@
+from flask import render_template
 from app import app
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'nickname': 'Stephan'}
+    posts = [
+  {
+    "author": {
+      "nickname": "Corrine Pitts"
+    },
+    "body": "Tempor anim dolore reprehenderit sit nisi cupidatat exercitation."
+  },
+  {
+    "author": {
+      "nickname": "Evangeline Reeves"
+    },
+    "body": "Eu consectetur irure eiusmod culpa enim ipsum et dolore labore."
+  },
+  {
+    "author": {
+      "nickname": "Rosalie Rosa"
+    },
+    "body": "Irure anim voluptate duis anim amet."
+  },
+  {
+    "author": {
+      "nickname": "Abigail Lawrence"
+    },
+    "body": "Non ad deserunt aliquip elit irure duis officia do ea."
+  },
+  {
+    "author": {
+      "nickname": "Hopper Hopkins"
+    },
+    "body": "Aute et duis deserunt elit id voluptate sit irure magna incididunt est sit anim ullamco."
+  }
+]
+    return render_template("index.html",
+                           title='Pycodeshare',
+                           user=user,
+                           posts=posts)
