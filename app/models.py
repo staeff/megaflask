349b7ac6a8a1d5435025c148058c24a0dafe1910
Stephan Klinger <staeff@arcor.de>
2015-02-15 22:08:52 +0000

    Snapshot pt. 8

--- app/models.py
+++ app/models.py
@@ -1,6 +1,16 @@
 from hashlib import md5
 from app import db
 
+# Note that we are not declaring this table as a model.
+# Since this is an auxiliary table that has no data other than the foreign
+# keys, we use the lower level APIs in flask-sqlalchemy to create the
+# table without an associated model.
+followers =db.Table(
+    'followers',
+    db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('followed_id', db.Integer, db.ForeignKey('user.id')),
+)
+
 
 class User(db.Model):
     """ Represents the user """
@@ -12,6 +22,12 @@ class User(db.Model):
     posts = db.relationship('Post', backref='author', lazy='dynamic')
     about_me = db.Column(db.String(140))
     last_seen = db.Column(db.DateTime)
+    followed = db.relationship('User',
+                                secondary=followers,
+                                primaryjoin=(followers.c.follower_id == id),
+                                secondaryjoin=(followers.c.followed_id == id),
+                                backref=db.backref('followers', lazy='dynamic'),
+                                lazy='dynamic')
 
     # Is a static method, since it this operation does not apply to any
     # particular instance of the class.
@@ -58,6 +74,31 @@ class User(db.Model):
     def avatar(self, size):
         return 'http://www.gravatar.com/avatar/%s?d=wavatar&s=%d' % \
              (md5(self.email.encode('utf-8')).hexdigest(), size)
+
+    def follow(self, user):
+        if not self.is_following(user):
+            self.followed.append(user)
+            return self
+
+    def unfollow(self, user):
+        if self.is_following(user):
+            self.followed.remove(user)
+            return self
+
+    def is_following(self, user):
+        """  Takes the followed relationship query, which returns all
+        the (follower, followed) pairs followed by user. Filter it by the
+        followed user. The followed relationship has a lazy mode of dynamic.
+        Its not the result of the query, its the actual query object 
+        before execution. """
+        return self.followed.filter(
+            followers.c.followed_id == user.id).count() > 0
+
+    def followed_posts(self):
+        return Post.query.join(
+            followers, (followers.c.followed_id == Post.user_id)).filter(
+                followers.c.follower_id == self.id).order_by(
+                    Post.timestamp.desc())
 
     def __repr__(self):
         return '<User %r>' % (self.nickname)
--- app/views.py
+++ app/views.py
@@ -93,6 +93,10 @@ def after_login(resp):
         user = User(nickname=nickname, email=resp.email)
         db.session.add(user)
         db.session.commit()
+        # make the user (implicitely) follow him/herself
+        # easily include the posts in the followed posts query -> tutorial pt. 8
+        db.session.add(user.follow(user))
+        db.session.commit()
     remember_me = False
     if 'remember_me' in session:
         remember_me = session['remember_me']
@@ -138,3 +142,33 @@ def edit():
         form.nickname.data = g.user.nickname
         form.about_me.data = g.user.about_me
     return render_template('edit.html', form=form)
+
+@app.route('/follow/<nickname>')
+def follow(nickname):
+    user = User.query.filter_by(nickname=nickname).first()
+    if user is None:
+        flash('User {0} not found.'.format(nickname))
+        return redirect(url_for('index'))
+    u = g.user.follow(user)
+    if u is None:
+        flash('Can not follow {0}.'.format(nickname))
+        return redirect(url_for('user', nickname=nickname))
+    db.session.add(u)
+    db.session.commit()
+    flash('You are now following {0}!'.format(nickname))
+    return redirect(url_for('user', nickname=nickname))
+
+@app.route('/unfollow/<nickname>')
+def unfollow(nickname):
+    user = User.query.filter_by(nickname=nickname).first()
+    if user is None:
+        flash('User {0} not found.'.format())
+        return redirect(url_for('index'))
+    u = g.user.unfollow(user)
+    if u is None:
+        flash('Can not unfollow {0}.'.format(nickname))
+        return redirect(url_for('user', nickname=nickname))
+    db.session.add(u)
+    db.session.commit()
+    flash('You have stopped following {0}.'.format(nickname))
+    return redirect(url_for('user', nickname=nickname))
