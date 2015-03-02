01087a765b15a73488c6b4d4fcf26f82c57cb0ad
Stephan Klinger <staeff@arcor.de>
2015-03-02 17:11:08 +0000

    Remove unused google translate function

--- app/translate.py
+++ app/translate.py
@@ -37,25 +37,3 @@ def microsoft_translate(text, sourceLang, destLang):
         return response_json["response"]
     except:
         raise
-
-
-def google_translate(text, sourceLang, destLang):
-    if not app.debug:
-        return gettext('Error: translation service not available.')
-    try:
-        params = urlencode({
-            'client': 't',
-            'text': text.encode("utf-8"),
-            'sl': sourceLang,
-            'tl': destLang,
-            'ie': 'UTF-8',
-            'oe': 'UTF-8'})
-        conn = httplib.HTTPSConnection("translate.google.com")
-        conn.request("GET", "/translate_a/t?" + params,
-                     headers={'User-Agent': 'Mozilla/5.0'})
-        httpresponse = conn.getresponse().read().replace(
-            ",,,", ",\"\",\"\",").replace(",,", ",\"\",")
-        response = json.loads("{\"response\":" + httpresponse + "}")
-        return response["response"][0][0][0]
-    except:
-        return gettext('Error: Unexpected error.')
